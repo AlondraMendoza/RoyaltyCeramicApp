d44f56de996c867c3fe7642d654d49929734bbba
Tania <ttania_torres@hotmail.com>
2017-10-07 11:23:34 -0600

    index

--- RoyaltyCeramic/index.php
+++ RoyaltyCeramic/index.php
@@ -3,7 +3,7 @@
 session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)) . DS);
-define('URL', "http://localhost/royaltyceramic/RoyaltyCeramicApp/royaltyceramic/");
+define('URL', "http://localhost/Proyectos/RoyaltyCeramicApp/royaltyceramic/");
 require_once 'Config/Autoload.php';
 Config\Autoload::run();
 if (!isset($_REQUEST["withouttem"])) {
